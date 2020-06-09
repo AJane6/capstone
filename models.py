c42522f91db73b63b3dde3e069ef2efb59b0aeb6
Ailsa <ailsa.robertson496@outlook.com>
2020-06-09 19:05:40 +0100

    updated migrations folder

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    #db_insert_records()
+    db_insert_records()
 
 
 def db_insert_records():
